43ecccdb9d3b4745dc5e48d2f511021119d21e4e
NadirZenith <2cb.md2@gmail.com>
2014-10-14 16:23:12 +0200

    added place-type tax

--- wp-content/themes/tm/lib/nz/post-types/top-place.php
+++ wp-content/themes/tm/lib/nz/post-types/top-place.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- *      fashion post type
+ *      top place post type
  */
 // Register Custom Post Type
 function register_top_place_post_type() {
@@ -27,7 +27,7 @@ function register_top_place_post_type() {
             'description' => __( 'Top Places post type', 'text_domain' ),
             'labels' => $labels,
             'supports' => array( 'title', 'editor', 'thumbnail', 'post-formats' ),
-            'taxonomies' => array( 'category', 'post_tag' ),
+            'taxonomies' => array('place-type'),
             'hierarchical' => false,
             'public' => true,
             'show_ui' => true,
@@ -47,3 +47,41 @@ function register_top_place_post_type() {
 
 // Hook into the 'init' action
 add_action( 'init', 'register_top_place_post_type', 0 );
+
+
+// Register Custom Taxonomy
+function place_type_taxonomy() {
+
+	$labels = array(
+		'name'                       => _x( 'Tipos de lugares', 'Taxonomy General Name', 'nz' ),
+		'singular_name'              => _x( 'Tipo de Lugar', 'Taxonomy Singular Name', 'nz' ),
+		'menu_name'                  => __( 'Place type', 'nz' ),
+		'all_items'                  => __( 'All Items', 'nz' ),
+		'parent_item'                => __( 'Parent Item', 'nz' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'nz' ),
+		'new_item_name'              => __( 'New Item Name', 'nz' ),
+		'add_new_item'               => __( 'Add New Item', 'nz' ),
+		'edit_item'                  => __( 'Edit Item', 'nz' ),
+		'update_item'                => __( 'Update Item', 'nz' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'nz' ),
+		'search_items'               => __( 'Search Items', 'nz' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'nz' ),
+		'choose_from_most_used'      => __( 'Choose from the most used items', 'nz' ),
+		'not_found'                  => __( 'Not Found', 'nz' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+        
+	register_taxonomy( 'place-type', array( 'top-place' ), $args );
+}
+
+
+// Hook into the 'init' action
+add_action( 'init', 'place_type_taxonomy', 0 );
