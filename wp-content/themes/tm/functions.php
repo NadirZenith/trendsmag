cc3d8945762a4fbc5109d27ba31b1a819e316dde
NadirZenith <2cb.md2@gmail.com>
2014-10-15 17:54:49 +0200

    added fancybox

--- wp-content/themes/tm/functions.php
+++ wp-content/themes/tm/functions.php
@@ -23,6 +23,7 @@ $roots_includes = array(
       'lib/comments.php', // Custom comments modifications
       'lib/scripts.php', // Scripts and stylesheets
       'lib/extras.php', // Custom functions
+      /*'nz/main.php', // Custom functions*/
 );
 
 foreach ( $roots_includes as $file ) {
