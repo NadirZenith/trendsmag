b66a0a7e451a8fcfd24ab8f2f6566cce02b51c82
NadirZenith <2cb.md2@gmail.com>
2014-10-27 15:27:38 +0100

    ..

--- wp-content/themes/tm/lib/nz/todo-pending-posts.php
+++ wp-content/themes/tm/lib/nz/todo-pending-posts.php
@@ -3,32 +3,30 @@
 add_action( 'admin_bar_menu', 'nz_toolbar_todo', 1050 );
 
 function nz_toolbar_todo( $wp_admin_bar ) {
-    
+
       $todo_total = 0;
       $post_types = array( 'event' );
-      $info_string = '<span style="color:red;">( %d )</span>';
+      $info_string = '<span style="color:red;"> ( %d ) </span>';
 
-      foreach ( $post_types as $key => $value ) {
+      foreach ( $post_types as $post_type ) {
             $posts = get_posts(
                       array(
                             'posts_per_page' => -1,
-                            'post_type' => $value,
+                            'post_type' => $post_type,
                             'post_status' => 'pending',
                             'suppress_filters' => true
                       )
             );
-            /* d($posts); */
             $total_posts = count( $posts );
-
             if ( $total_posts > 0 ) {
 
                   $url = ($total_posts == 1) ?
                             get_edit_post_link( $posts[ 0 ]->ID ) :
-                            admin_url( 'edit.php?post_status=pending&post_type=' . $key );
+                            admin_url( 'edit.php?post_status=pending&post_type=' . $post_type );
 
                   $args = array(
-                        'id' => 'pending-' . $key,
-                        'title' => $key . sprintf( $info_string, $total_posts ), // alter the title of existing node
+                        'id' => 'pending-' . $post_type,
+                        'title' => $post_type . sprintf( $info_string, $total_posts ), // alter the title of existing node
                         'href' => $url,
                         'parent' => 'nz-todo', // set parent to false to make it a top level (parent) node
                   );
