2ee76830dcec0cbf3051514458309cd9f08de94a
NadirZenith <2cb.md2@gmail.com>
2014-10-27 11:01:28 +0100

    ..

--- wp-content/themes/tm/lib/nz/assets.php
+++ wp-content/themes/tm/lib/nz/assets.php
@@ -1,5 +1,11 @@
 <?php
 
+function is_localhost() {
+      $whitelist = array( '127.0.0.1', '::1' );
+      if ( in_array( $_SERVER[ 'REMOTE_ADDR' ], $whitelist ) )
+            return true;
+}
+
 function nz_get_asset( $type, $asset = null ) {
       if ( $asset ) {
             if ( is_localhost() ) {
