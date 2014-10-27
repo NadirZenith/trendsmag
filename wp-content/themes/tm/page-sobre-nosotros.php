66f64feb41f17a25d5fc8ff16cf017c51415cb5c
NadirZenith <2cb.md2@gmail.com>
2014-10-27 17:59:06 +0100

    ..

--- wp-content/themes/tm/page-sobre-nosotros.php
+++ wp-content/themes/tm/page-sobre-nosotros.php
@@ -1,6 +1,6 @@
 <div class="row">
 
-      <div class="col-xs-12 col-md-8">
+      <div class="col-xs-12 col-md-10">
             <?php while ( have_posts() ) : the_post(); ?>
                   <?php get_template_part( 'templates/page', 'header' ); ?>
                   <?php get_template_part( 'templates/content', 'page' ); ?>
