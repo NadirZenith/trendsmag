8862c4fb90ec47aef33c4c292f74537f0cdeb2b7
NadirZenith <2cb.md2@gmail.com>
2014-10-27 17:58:13 +0100

    ..

--- wp-content/themes/tm/page-sobre-nosotros.php
+++ wp-content/themes/tm/page-sobre-nosotros.php
@@ -1,7 +1,12 @@
-<?php while ( have_posts() ) : the_post(); ?>
-      <?php get_template_part( 'templates/page', 'header' ); ?>
-      <?php get_template_part( 'templates/content', 'page' ); ?>
-<?php endwhile; ?>
+<div class="row">
+
+      <div class="col-xs-12 col-md-8">
+            <?php while ( have_posts() ) : the_post(); ?>
+                  <?php get_template_part( 'templates/page', 'header' ); ?>
+                  <?php get_template_part( 'templates/content', 'page' ); ?>
+            <?php endwhile; ?>
+      </div>
+</div>
 
 <h1>STAFF</h1>
 
