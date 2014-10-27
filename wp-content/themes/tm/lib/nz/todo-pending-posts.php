107293a9c4ed6c535304467f14262040a6903d6a
NadirZenith <2cb.md2@gmail.com>
2014-10-27 14:31:43 +0100

    added

---
+++ wp-content/themes/tm/lib/nz/todo-pending-posts.php
@@ -0,0 +1,47 @@
+<?php
+
+add_action( 'admin_bar_menu', 'nz_toolbar_todo', 1050 );
+
+function nz_toolbar_todo( $wp_admin_bar ) {
+    
+      $todo_total = 0;
+      $post_types = array( 'event' );
+      $info_string = '<span style="color:red;">( %d )</span>';
+
+      foreach ( $post_types as $key => $value ) {
+            $posts = get_posts(
+                      array(
+                            'posts_per_page' => -1,
+                            'post_type' => $value,
+                            'post_status' => 'pending',
+                            'suppress_filters' => true
+                      )
+            );
+            /* d($posts); */
+            $total_posts = count( $posts );
+
+            if ( $total_posts > 0 ) {
+
+                  $url = ($total_posts == 1) ?
+                            get_edit_post_link( $posts[ 0 ]->ID ) :
+                            admin_url( 'edit.php?post_status=pending&post_type=' . $key );
+
+                  $args = array(
+                        'id' => 'pending-' . $key,
+                        'title' => $key . sprintf( $info_string, $total_posts ), // alter the title of existing node
+                        'href' => $url,
+                        'parent' => 'nz-todo', // set parent to false to make it a top level (parent) node
+                  );
+                  $todo_total += $total_posts;
+
+                  $wp_admin_bar->add_node( $args );
+            }
+      }
+      $args = array(
+            'id' => 'nz-todo',
+            'title' => '@Todo ' . sprintf( $info_string, $todo_total ),
+            'meta' => array( 'class' => 'nz-toolbar-todo' )
+      );
+
+      $wp_admin_bar->add_node( $args );
+}
