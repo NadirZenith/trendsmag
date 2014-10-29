482b8a99d74ff574fc3971160828bf5c66b7b84a
NadirZenith <2cb.md2@gmail.com>
2014-10-29 13:34:26 +0100

    ..

--- wp-content/themes/tm/templates/base/head.php
+++ wp-content/themes/tm/templates/base/head.php
@@ -3,13 +3,13 @@
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <title><?php wp_title( '|', true, 'right' ); ?></title>
-      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
       <?php wp_head(); ?>
 
       <?php
       /*
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
        */
       ?>
