d6040b7fa9dc4822062aec346536be3f185c116e
NadirZenith <2cb.md2@gmail.com>
2014-10-29 14:11:21 +0100

    ..

--- wp-content/themes/tm/lib/nz/mail-redirect.php
+++ wp-content/themes/tm/lib/nz/mail-redirect.php
@@ -2,15 +2,11 @@
 
 function nz_mail_redirect( $wp ) {
       if (
-      /* is_404() && $wp->request == 'mail' */
-                is_page( 'mail' )
+                is_404() && $wp->request == 'mail'
       ) {
-            global $wp_query;
-            d( $wp_query );
+            wp_redirect( 'https://mail.zoho.com/portal/trendsmag' );
       }
-
-      wp_redirect( 'https://mail.zoho.com/portal/trendsmag' );
 }
 
-/*add_action( 'wp', 'nz_mail_redirect' );*/
+add_action( 'wp', 'nz_mail_redirect' );
 ?>
