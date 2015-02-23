1d09ba173d3665dc348e76c9a08cec1e65be8290
NadirZenith <2cb.md2@gmail.com>
2015-02-23 13:00:07 +0100

    update fb

--- wp-content/themes/tm/templates/sidebar-single.php
+++ wp-content/themes/tm/templates/sidebar-single.php
@@ -10,32 +10,18 @@
 */
 ?>
 <div class="aside-item">
-      <?php nz_fb_like_box( 'https://www.facebook.com/trendsmag.net' ); ?>
+      <?php echo nz_fb_like_box( 'https://www.facebook.com/trendsmag.net' ); ?>
 </div>
-<?php 
-/*
-<div class="aside-item">
-      <?php nz_fb_sharer( 'https://www.facebook.com/trendsmag.net' ); ?>
-</div>
-<div class="aside-item">
-      <a href="whatsapp://send?text=Hello%2C%20World!">
-            whatsapp
-      </a>
-</div>
- */
-?>
+
 
 <?php dynamic_sidebar( 'sidebar-primary' ); ?>
 
 
 <script>
-/*      
- * */
       jQuery(function($) {
             $("aside.sidebar").stick_in_parent({
                   offset_top: 70
             });
-            // call my method
 
       });
 </script>
