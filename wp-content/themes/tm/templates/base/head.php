230d1dd26cca7008d94fb83edf7ece531c12321c
NadirZenith <2cb.md2@gmail.com>
2015-03-20 15:02:16 +0100

    google verification

--- wp-content/themes/tm/templates/base/head.php
+++ wp-content/themes/tm/templates/base/head.php
@@ -4,7 +4,7 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <title><?php wp_title( '|', true, 'right' ); ?></title>
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-
+      <meta name="google-site-verification" content="O4MngPCXywvE0LRmRJk7VBTg3ZM0ge6ybviyNcpSHzM" />
       <?php wp_head(); ?>
 
       <?php
