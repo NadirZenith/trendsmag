ae3297396809684588a463e7c32875333ac285cd
NadirZenith <2cb.md2@gmail.com>
2015-04-01 04:03:50 +0200

    ..

--- wp-content/themes/tm/templates/sidebar-single.php
+++ wp-content/themes/tm/templates/sidebar-single.php
@@ -1,27 +1,16 @@
-<?php 
-/*
 <div class="aside-item">
-      <?php nz_fb_like(); ?>
+    <?php echo nz_fb_like_box('https://www.facebook.com/trendsmag.net'); ?>
 </div>
-<div class="aside-item">
-      <?php nz_tt_tweet(); ?>
 
-</div>
-*/
+<?php
+dynamic_sidebar('sidebar-primary');
 ?>
-<div class="aside-item">
-      <?php echo nz_fb_like_box( 'https://www.facebook.com/trendsmag.net' ); ?>
-</div>
-
-
-<?php dynamic_sidebar( 'sidebar-primary' ); ?>
-
 
 <script>
-      jQuery(function($) {
-            $("aside.sidebar").stick_in_parent({
-                  offset_top: 70
-            });
+    jQuery(function ($) {
+        $("aside.sidebar").stick_in_parent({
+            offset_top: 70
+        });
 
-      });
+    });
 </script>
