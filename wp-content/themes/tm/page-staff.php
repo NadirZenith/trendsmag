8297612d46dbeefb6aa12b9e80b5237cd22b91ac
NadirZenith <2cb.md2@gmail.com>
2015-05-08 17:28:10 +0200

    remove staff

--- wp-content/themes/tm/page-staff.php
+++ wp-content/themes/tm/page-staff.php
@@ -39,39 +39,7 @@
         </article>
 
     </li>
-    <?php
-    /*
-
-      ?>
-      <li class="row">
-      <article>
-      <div class="col-md-2">
-      <img src="<?php echo nz_get_image_asset('staff/macarena.jpg') ?>" class="img-circle"/>
-      </div>
-      <div class="col-md-10">
-      <h2> Macarena </h2>
-      <span class="text-italic">
-      Editora / Relaciones publicas
-      </span>
-      <p>
-      <b>
-      FRAN, Nunca pasará de moda, porque siempre conoce la moda.
-      </b>
-      <br>
-      Analista de tendencias especializada en culturas juveniles y tribus urbanas, actualmente enfoca su trabajo hacia el análisis de moda. Se formó como Coolhunter en la agencia DMentes, donde descubrió que el mundo de las tendencias es su verdadero camino.
-
-      Fran es nuestra experta en moda, pero sus inquietudes la convierten, sin duda, en una inagotable fuente de conocimiento acerca de todo lo que puede catalogarse como “urbano”: arte, style, música, deporte…
-
-      Seguramente la veréis más de una vez investigando por el Raval en su querida bicicleta.
-      </p>
-      </div>
-
-      </article>
-
-      </li>
-      <?php
-     */
-    ?>
+    
     <li class="row">
         <article>
 
