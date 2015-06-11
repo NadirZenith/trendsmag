85bbad74a67ca48e1c65d10103197568cd11dedf
NadirZenith <2cb.md2@gmail.com>
2015-06-11 20:22:52 +0200

    updated buy tickets btn

--- wp-content/themes/tm/templates/base/header.php
+++ wp-content/themes/tm/templates/base/header.php
@@ -13,8 +13,8 @@
                     <?php echo nz_fb_like_iframe('https://www.facebook.com/trendsmag.net'); ?>
                     <?php get_template_part('templates/nz/social/social-icons-top'); ?>
                 </div>
-                <a href="<?php echo get_permalink(get_page_by_path('entradas')) ?>" class="pull-right btn btn-default">
-                    comprar entradas
+                <a href="<?php echo get_permalink(get_page_by_path('entradas')) ?>" class="pull-right btn btn-default2 btn-buytickets">
+                    TREND tickets
                 </a>
             </div>
         </div>
